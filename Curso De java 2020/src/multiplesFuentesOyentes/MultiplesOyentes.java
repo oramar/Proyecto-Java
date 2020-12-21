e47416e6f55977ecdf05d53d838225f423de0b59
Orlando Amado Ardila <Orlando-amado@hotmail.com>
2020-12-21 07:57:26 -0500

    Se termina el ejermplo de la clase multiplesOyentes()

--- Curso De java 2020/src/multiplesFuentesOyentes/MultiplesOyentes.java
+++ Curso De java 2020/src/multiplesFuentesOyentes/MultiplesOyentes.java
@@ -1,14 +1,70 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package multiplesFuentesOyentes;
 
-/**
- *
- * @author Orlando
- */
+import formularios.MarcoCentrado;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
 public class MultiplesOyentes {
     
+    public static void main(String[] args) {
+        MarcoCentrado marco = new MarcoCentrado();
+        //primero agrego la lamina antes de crear el marco
+        panelAccionOyente lamina = new panelAccionOyente();
+        marco.add(lamina);
+        marco.anchoAlto(50, 50);
+        marco.crearFormulario("Multiples Oyentes");
+    }
+}
+
+class panelAccionOyente extends JPanel {
+    
+    private final JButton CerrarToto;
+    private final JButton CrearVentana;
+    
+    public panelAccionOyente() {
+        CerrarToto = new JButton("Cerrar Todo");
+        CrearVentana = new JButton("Crear Ventana");        
+        add(CrearVentana);
+        add(CerrarToto);
+        OyenteNuevo oyenteNuevo = new OyenteNuevo();
+        CrearVentana.addActionListener(oyenteNuevo);
+        
+    }
+    
+    private class OyenteNuevo implements ActionListener {
+        
+        @Override
+        public void actionPerformed(ActionEvent ae) {
+            MarcoNuevo marco = new MarcoNuevo(CerrarToto);
+            marco.setVisible(true);
+            
+        }
+        
+    }
+    
+}
+
+class MarcoNuevo extends JFrame {
+
+    private static int contador = 0;
+
+    public MarcoNuevo(JButton CerrarToto) {
+        contador++;
+        setTitle("Venta " + contador);
+        setBounds(40 * contador, 40 * contador, 300, 150);
+        CierraTodo cierraTodo = new CierraTodo();
+        CerrarToto.addActionListener(cierraTodo);
+    }
+    
+    private class CierraTodo implements ActionListener{
+
+        @Override
+        public void actionPerformed(ActionEvent ae) {
+            dispose();
+        }
+        
+    }
 }
