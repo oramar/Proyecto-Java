40d7203e420edda409b39d58e7a4782ed1ad8bd4
Orlando Amado Ardila <Orlando-amado@hotmail.com>
2021-01-01 14:22:49 -0500

    Aprendiendo el uso de menus en Frame

---
+++ Curso De java 2020/src/componentesSwing/ConstruyendoMenus.java
@@ -0,0 +1,14 @@
+
+package componentesSwing;
+
+import formularios.MarcoCentrado;
+
+public class ConstruyendoMenus {
+    public static void main(String[] args) {
+        //Video 100
+        MarcoCentrado marco = new MarcoCentrado();
+        marco.anchoAlto(20, 50);
+        marco.crearFormulario("Trabajando con Menus");
+    }
+          
+}
