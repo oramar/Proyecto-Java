300eaa53745b5e58bbe08102700e5ebe2e178be1
Orlando Linux <armando0719@hotmail.com>
2021-01-07 23:44:56 -0500

    Aprendiendo disposiciones avanzadas En esta ocasion aprenderemos a utilizar las disposiciones avanzadas.

---
+++ Curso De java 2020/src/dispocisionesAvanzadas/DisposicionCaja.java
@@ -0,0 +1,14 @@
+
+package dispocisionesAvanzadas;
+
+import formularios.MarcoCentrado;
+
+
+public class DisposicionCaja {
+    public static void main(String[] args) {
+        MarcoCentrado marco = new MarcoCentrado();
+        marco.anchoAlto(50, 50);
+        marco.crearFormulario("Disposiciones en caja");
+        
+    }
+}
