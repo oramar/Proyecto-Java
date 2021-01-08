9a2843e6095675bdf5d61982db476a8e5eceb909
Orlando Linux <armando0719@hotmail.com>
2021-01-07 22:40:29 -0500

    Adecuando el formulario centrado a cualquier sistema operativo Se agregar la instruccion en la posicion adecuada y se anexa el otro metodo de centrar el formulario

--- Curso De java 2020/src/formularios/MarcoCentrado.java
+++ Curso De java 2020/src/formularios/MarcoCentrado.java
@@ -1,4 +1,3 @@
-
 package formularios;
 
 import java.awt.Color;
@@ -7,7 +6,8 @@ import java.awt.Image;
 import java.awt.Toolkit;
 import javax.swing.JFrame;
 
-public class MarcoCentrado extends JFrame{
+public class MarcoCentrado extends JFrame {
+
     private int ancho;
     private int alto;
     private int posicionAlto;
@@ -15,33 +15,35 @@ public class MarcoCentrado extends JFrame{
     //Con toolkit y el metodo getDefaulToolkit() obtenemos acceso a nuestro sistema huespede
     Toolkit miPantalla = Toolkit.getDefaultToolkit();
     Dimension tamanoPantalla = miPantalla.getScreenSize();
-    
-    public MarcoCentrado(){
-         miPantalla.beep();
-        Image imagen = miPantalla.getImage("src/swing/img/usuario.png");        
+
+    public MarcoCentrado() {
+        miPantalla.beep();
+        Image imagen = miPantalla.getImage("src/swing/img/usuario.png");
         setIconImage(imagen);
-      
-       
+
     }
-    
-      public void anchoAlto(int ancho,int alto){
-        
+
+    public void anchoAlto(int ancho, int alto) {
+
         this.ancho = tamanoPantalla.width;
         this.alto = tamanoPantalla.height;
-        double anchoDecimal = (double)this.ancho/100*ancho;
-        double altoDecimal = (double)this.alto/100*alto;
-        this.posicionAncho = (int)((this.ancho-anchoDecimal)/2);
-        this.posicionAlto=(int)((this.alto-altoDecimal)/2);
-        this.alto  = (int)altoDecimal;
-       this.ancho  = (int)anchoDecimal;
-          
+        double anchoDecimal = (double) this.ancho / 100 * ancho;
+        double altoDecimal = (double) this.alto / 100 * alto;
+        this.posicionAncho = (int) ((this.ancho - anchoDecimal) / 2);
+        this.posicionAlto = (int) ((this.alto - altoDecimal) / 2);
+        this.alto = (int) altoDecimal;
+        this.ancho = (int) anchoDecimal;
+
     }
-       
-    public void crearFormulario(String Titulo){
+
+    public void crearFormulario(String Titulo) {
         setVisible(true);
-        setBounds(posicionAncho,posicionAlto,ancho,alto);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setTitle(Titulo);
         setResizable(false);
+        setBounds(posicionAncho, posicionAlto, ancho, alto);
+        //Otra forma de centrarlo es
+        //setSize(ancho,alto);
+        //setLocationRelativeTo(null);
     }
 }
